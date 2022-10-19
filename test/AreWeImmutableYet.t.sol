2a229058d3aed0d036a13b776814577f40fcc11a
James Wenzel <james.wenzel@opensea.io>
2022-10-19 11:37:41 -0700

    add test case for known bad opcode

--- test/AreWeImmutableYet.t.sol
+++ test/AreWeImmutableYet.t.sol
@@ -4,14 +4,31 @@ pragma solidity ^0.8.13;
 import "forge-std/Test.sol";
 import "../src/AreWeImmutableYet.sol";
 
+contract AreWeBadOpcodeYet {
+    /**
+     * known bad opcode
+     */
+    function areWeImmutableYet() public returns (bool weAreImmutable) {
+        ///@solidity memory-safe-assembly
+        assembly {
+            mstore(0, 0x600b5981380380925939f333EE)
+            let addr := create2(0, 19, 13, 0)
+            weAreImmutable := iszero(call(5010, addr, 0, 0, 0, 0, 0))
+        }
+    }
+}
+
 contract CounterTest is Test {
-    AreWeImmutableYet public counter;
+    AreWeImmutableYet public areWeImmutableYet;
+    AreWeBadOpcodeYet public areWeBadOpcodeYet;
 
     function setUp() public {
-        counter = new AreWeImmutableYet();
+        areWeImmutableYet = new AreWeImmutableYet();
+        areWeBadOpcodeYet = new AreWeBadOpcodeYet();
     }
 
     function testAreWeImmutableYet() public {
-        assertFalse(counter.areWeImmutableYet());
+        assertFalse(areWeImmutableYet.areWeImmutableYet());
+        assertTrue(areWeBadOpcodeYet.areWeImmutableYet());
     }
 }
